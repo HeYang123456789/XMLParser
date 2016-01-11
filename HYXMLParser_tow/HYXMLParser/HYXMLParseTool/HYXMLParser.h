fb78ef852d71b72ddd1163af7d5ca55cc90be2e2
HeYang123456789 <1554566348@qq.com>
2016-01-11 23:47:58 +0800

    two

---
+++ HYXMLParser_tow/HYXMLParser/HYXMLParseTool/HYXMLParser.h
@@ -0,0 +1,23 @@
+//
+//  XMLParse.h
+//  Network
+//
+//  Created by HEYANG on 15/12/27.
+//  Copyright © 2015年 HEYANG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HYXMLParser : NSObject
+
+#pragma mark - Create a object
+/** 初始化方法，通过传入模型对象的类名，创建这个XML解析对象 */
+- (instancetype)initWithModelClassName:(NSString*)modelClassName withElementName:(NSString*)elementName;
+/** 便利构造器类方法，通过传入模型对象的类名，创建这个XML解析对象 */
++(instancetype)hyXMLParseWithModelClassName:(NSString*)modelClassName withElementName:(NSString*)elementName;
+
+#pragma mark - parser xml data
+/** 通过NSData解析XML数据，返回模型数组 */
+-(NSMutableArray*)objectFromData:(NSData*)data;
+
+@end
---
+++ HYXMLParser_tow/HYXMLParser/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  HYXMLParser
+//
+//  Created by HEYANG on 15/12/28.
+//  Copyright © 2015年 HEYANG. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "HYXMLParser.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //xml -> NSData
+    NSString* dataXmlPath = [[NSBundle mainBundle] pathForResource:@"data.xml" ofType:nil];
+    NSData* data = [NSData dataWithContentsOfFile: dataXmlPath];
+    
+    
+    //假设这个data是来自服务器端，那么我们就直接使用HYXMLParse解析这个data
+    
+    //1、创建HYXMLParse对象，需要传入Model Class Name 和 XML 子节点的名 video
+    HYXMLParser* parser = [[HYXMLParser alloc] initWithModelClassName:@"VideoModel" withElementName:@"video"];
+    
+    //NSData -> NSMutableArray of models
+    NSMutableArray* models = [parser objectFromData:data];
+    
+    NSLog(@"%@",models);
+}
+
+
+
+@end
